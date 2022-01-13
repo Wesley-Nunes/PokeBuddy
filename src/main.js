f9df0da1ecd0edcc84cfcd172b138abe0e7f556b
Wesley-Nunes <wesley_info@yahoo.com.br>
2022-01-13 17:19:15 -0300

    refactor: Improve the structure of html, css and js files

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import playerAction from "./services/playerAction.js";
 
-const pokeball = document.querySelector("#pokeball");
-const closeBtn = document.querySelector("#close-btn");
-const chooseAnother = document.querySelector("#choose-another");
-const confirmAdventure = document.querySelector("#confirm-adventure");
+const pokeball = document.querySelector(".pokeball");
+const closeBtn = document.querySelector(".close-btn");
+const chooseAnother = document.querySelector(".choose-another");
+const confirmAdventure = document.querySelector(".confirm-adventure");
 
 pokeball.addEventListener("click", () => playerAction.openPokeball());
 
--- src/services/playerAction.js
+++ src/services/playerAction.js
@@ -1,34 +1,37 @@
 import pokeApiRequest from "./pokeApiRequest.js";
 
-function modifyStyle(parent, child) {
-  let section = document.querySelector("#pokemon-section");
-
+function modifyStyle(parent, firstChild, secondChild) {
+  let section = document.querySelector(".pokemon-section");
+  let pokemonInfo = document.querySelector(".pokemon-info");
+  let textSection = document.querySelector(".adventure");
+  
   section.style.display = parent;
-  section.children[1].style.display = child;
+  pokemonInfo.style.display = firstChild;
+  textSection.style.display = secondChild;
 }
 
 function modifyHtml(values) {
-  document.querySelector("#img").src = values[0];
+  document.querySelector("#pokemon-img").src = values[0];
   document.querySelector("#pokemon-icon").src = values[1];
-  document.querySelector("#poke-name").textContent = values[2];
-  document.querySelector("#choosed-poke-name").textContent = values[2];
-  document.querySelector("#poke-type").textContent = values[3];
+  document.querySelector("#pokemon-name").textContent = values[2];
+  document.querySelector("#choosed-pokemon-name").textContent = values[2];
+  document.querySelector("#pokemon-type").textContent = values[3];
 }
 
 class playerAction {
   openPokeball() {
-    modifyStyle("block", "none");
+    modifyStyle("block", "block", "none");
     pokeApiRequest().then((response) => modifyHtml(response));
   }
 
   closePokeball() {
-    modifyStyle("none", "none");
+    modifyStyle("none", "none", "none");
     let values = ["./src/media/loading.webp", "./src/media/pokeball-icon.webp", "", "", ""];
     modifyHtml(values);
   }
 
   startAdventure() {
-    modifyStyle("block", "block");
+    modifyStyle("block", "none", "block");
   }
 }
 
